97452a3a105a4e84b12fa537ead96361d7fed8a4
am <adrianmarino10@gmail.com>
2024-06-07 12:57:43 +0200

    all frontend

---
+++ homepage.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('Homepage')
+# URL of the logo image
+logo_url = "https://upload.wikimedia.org/wikipedia/commons/b/b4/Eurowings_Logo.svg"
+# Display the logo
+st.image(logo_url, width=400)
